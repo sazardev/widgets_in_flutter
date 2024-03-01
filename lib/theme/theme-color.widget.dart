6968dc4c277940baa49af049e7dc1100bfa76e5e
Omar Flores <cerberusprogrammer@gmail.com>
2024-03-01 09:45:43 -0800

    New modular theme color changer controller!

--- lib/theme/theme-color.widget.dart
+++ lib/theme/theme-color.widget.dart
@@ -1,100 +1,78 @@
+// theme-selector.widget.dart
 import 'package:flutter/material.dart';
 import 'package:widgets_in_flutter/theme/theme.controller.dart';
+
+final Map<Color, String> colorList = {
+  Colors.blue: 'Blue',
+  Colors.red: 'Red',
+  Colors.green: 'Green',
+  Colors.orange: 'Orange',
+  Colors.purple: 'Purple',
+  Colors.brown: 'Brown',
+  Colors.grey: 'Grey',
+  Colors.teal: 'Teal',
+  Colors.cyan: 'Cyan',
+  Colors.indigo: 'Indigo',
+};
 
 class ThemeSelector extends StatelessWidget {
   const ThemeSelector({
     super.key,
-    required ThemeController themeController,
-  }) : _themeController = themeController;
+    required this.themeController,
+    required this.isLandscape,
+  });
 
-  final ThemeController _themeController;
+  final ThemeController themeController;
+  final bool isLandscape;
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<Color>(
-      icon: const Icon(Icons.color_lens),
-      onSelected: (Color color) {
-        _themeController.changePrimaryColor(color);
-      },
-      itemBuilder: (BuildContext context) => const <PopupMenuEntry<Color>>[
-        PopupMenuItem<Color>(
-          value: Colors.blue,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.blue),
-            title: Text('Blue'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.red,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.red),
-            title: Text('Red'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.green,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.green),
-            title: Text('Green'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.yellow,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.yellow),
-            title: Text('Yellow'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.orange,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.orange),
-            title: Text('Orange'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.purple,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.purple),
-            title: Text('Purple'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.brown,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.brown),
-            title: Text('Brown'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.grey,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.grey),
-            title: Text('Grey'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.teal,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.teal),
-            title: Text('Teal'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.cyan,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.cyan),
-            title: Text('Cyan'),
-          ),
-        ),
-        PopupMenuItem<Color>(
-          value: Colors.indigo,
-          child: ListTile(
-            leading: Icon(Icons.palette_outlined, color: Colors.indigo),
-            title: Text('Indigo'),
-          ),
-        ),
-      ],
-    );
+    return isLandscape
+        ? SizedBox(
+            height: 200,
+            child: GridView.count(
+              crossAxisCount: 5,
+              children: colorList.entries.map((entry) {
+                return Radio<Color>(
+                  value: entry.key,
+                  groupValue: themeController.primaryColor.value,
+                  onChanged: (Color? color) {
+                    themeController.changePrimaryColor(color!);
+                  },
+                  activeColor: entry.key,
+                  hoverColor: entry.key.withOpacity(0.1),
+                  focusColor: entry.key,
+                  fillColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                    if (states.contains(MaterialState.disabled)) {
+                      return entry.key.withOpacity(.32);
+                    }
+                    return entry.key;
+                  }),
+                );
+              }).toList(),
+            ),
+          )
+        : PopupMenuButton<Color>(
+            icon: const Icon(Icons.color_lens),
+            onSelected: (Color color) {
+              themeController.changePrimaryColor(color);
+            },
+            itemBuilder: (BuildContext context) {
+              return colorList.entries.map((entry) {
+                return PopupMenuItem<Color>(
+                  value: entry.key,
+                  child: ListTile(
+                    leading: Icon(Icons.circle, color: entry.key),
+                    title: Text(entry.value),
+                  ),
+                );
+              }).toList();
+            },
+          );
+  }
+
+  bool useWhiteForeground(Color color) {
+    return 1.05 / (color.computeLuminance() + 0.05) > 4.5;
   }
 }
