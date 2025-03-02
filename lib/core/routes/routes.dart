967607efe8c903ec809efaa30a8fc68d60b29cdd
sazardev <sazardev@gmail.com>
2025-03-02 11:49:46 -0800

    Add FAVORITES route to the application routing

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,6 +1,7 @@
 class Routes {
   static const INITIAL = '/components';
   static const SETTINGS = '/settings';
+  static const FAVORITES = '/favorites';
   static const COMPONENTS_ACTIONS = '/components/actions/';
   static const COMPONENTS_SETTINGS = '/components/settings/';
 }
