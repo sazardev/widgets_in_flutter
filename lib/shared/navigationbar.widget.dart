96f7758eb11ca426919ff68c039a615f59239bf6
sazardev <sazardev@gmail.com>
2025-03-02 11:50:04 -0800

    Add navigation options for Favorites and Search screens in NavigationBarWidget

--- lib/shared/navigationbar.widget.dart
+++ lib/shared/navigationbar.widget.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:widgets_in_flutter/favorites/favorites.screen.dart';
 import 'package:widgets_in_flutter/widgets/components.screen.dart';
 import 'package:widgets_in_flutter/core/routes/routes.controller.dart';
 import 'package:widgets_in_flutter/settings/settings.screen.dart';
+import 'package:widgets_in_flutter/widgets/search.screen.dart';
 
 class NavigationBarWidget extends StatelessWidget {
   const NavigationBarWidget({super.key});
@@ -15,7 +17,15 @@ class NavigationBarWidget extends StatelessWidget {
           destinations: const <NavigationDestination>[
             NavigationDestination(
               icon: Icon(Icons.widgets),
-              label: 'Components',
+              label: 'Widgets',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.favorite),
+              label: 'Favorites',
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.search),
+              label: 'Search',
             ),
             NavigationDestination(
               icon: Icon(Icons.settings),
@@ -31,6 +41,14 @@ class NavigationBarWidget extends StatelessWidget {
                     transition: Transition.noTransition);
                 break;
               case 1:
+                Get.to((() => const FavoritesScreen()),
+                    transition: Transition.rightToLeftWithFade);
+                break;
+              case 2:
+                Get.to((() => const SearchScreen()),
+                    transition: Transition.rightToLeftWithFade);
+                break;
+              case 3:
                 Get.offAll((() => const SettingsScreen()),
                     transition: Transition.noTransition);
                 break;
